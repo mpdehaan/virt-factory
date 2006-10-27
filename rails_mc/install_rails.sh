33d1d1cf59cf75135c9a0d960c2e7bb8dc6715d1
Michael DeHaan <mdehaan@redhat.com>
2006-10-27 13:58:42 -0400

    In the middle of database stuff

--- rails_mc/install_rails.sh
+++ rails_mc/install_rails.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum -y install ruby ruby-libs ruby-rdoc ruby-irb ruby-ri ruby-docs ruby-mode
+yum -y install ruby ruby-libs ruby-rdoc ruby-irb ruby-ri ruby-docs ruby-mode ruby-sqlite3 sqlite
 wget http://rubyforge.org/frs/download.php/11289/rubygems-0.9.0.tgz
 gunzip rubygems-0.9.0.tgz
 tar -xvf rubygems-0.9.0.tar
