782f8885edeaa748294dc78e195f83085e6f230b
Michael DeHaan <mdehaan@redhat.com>
2006-10-30 12:27:05 -0500

    Move permission code into helper.

--- rails_mc/src/app/controllers/demo_controller.rb
+++ rails_mc/src/app/controllers/demo_controller.rb
@@ -1,13 +1,14 @@
+
 class DemoController < ApplicationController
+
+   include ApplicationHelper
 
    def index
       redirect_to :action => 'list'
    end
 
    def list
-      unless @session[:login]
-         redirect_to :controller=>"login", :action=>"input"
-      end
+      require_permission(0)
       @items = ["audit","event","deployment","deployment_value",
                 "event","feature","field_type","image","image_value",
                 "machine","machine_value","metric","permission",
--- rails_mc/src/app/helpers/application_helper.rb
+++ rails_mc/src/app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+   def require_permission(feature)
+      # FIXME: actually pay attention to feature input
+      unless @session[:login]
+         redirect_to :controller => "login", :action => "input"
+      end
+   end
+
 end
