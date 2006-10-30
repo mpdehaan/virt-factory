50f9c669adff0c6fd53563e7a116c8e725c44046
Michael DeHaan <mdehaan@redhat.com>
2006-10-30 12:29:10 -0500

    Remove some prints

--- rails_mc/src/app/controllers/login_controller.rb
+++ rails_mc/src/app/controllers/login_controller.rb
@@ -5,21 +5,17 @@ class LoginController < ApplicationController
    end
 
    def input
+      # this is just a form.  No need for database loads yet.
    end
 
    def submit
       f_username = @params["form"]["username"]
       f_password = @params["form"]["password"]
-      puts ",#{f_username},"
-      puts ",#{f_password},"
       item = User.find_by_username(f_username)
       if item.nil?
-          print "is nil"
           redirect_to :action => "input"
           return
       end
-      puts item
-      puts item.password
       if item.password == f_password
           @session[:login] = f_username
           redirect_to :controller => "demo", :action => "list"
