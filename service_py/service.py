8f7a642e3a328f870047771cc766a3c9315060c3
mdehaan@mdehaan.rdu.redhat.com <>
2006-11-03 11:00:57 -0500

    Add python equivalent web service.

---
+++ service_py/service.py
@@ -0,0 +1,23 @@
+#from pysqlite2 import dbapi2 as sqlite # actually db is sqlite 3.0
+import sqlite
+import SimpleXMLRPCServer
+
+class XmlRpcInterface:
+
+   def __init__(self):
+       self.conn = sqlite.connect("/opt/shadowmanager/primary_db")
+       self.cursor = self.conn.cursor()
+
+   def login(self,user,password):
+       # FIXME: placeholders
+       self.cursor.execute("SELECT * FROM USERS WHERE username='%s' AND password='%s'" % (user,password))  
+       results = self.cursor.fetchall()
+       if len(results) >=1:
+           return results[0][0]
+       else:
+           return -1
+
+xmlrpc_interface = XmlRpcInterface()
+server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 5150))
+server.register_instance(xmlrpc_interface)
+server.serve_forever()
--- shadowmanager/test_service.rb
+++ shadowmanager/test_service.rb
@@ -3,4 +3,5 @@ require "xmlrpc/client"
 server = XMLRPC::Client.new("127.0.0.1","/",5150)
 
 puts server.call("login","guest","guest")
+puts server.call("login","guest2","guest")
 
