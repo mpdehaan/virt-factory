4a99e337cc32c7e7d8407ed20b6e4018dede3d37
Michael DeHaan <mdehaan@redhat.com>
2006-11-16 12:41:07 -0500

    Adding a place for shared error codes.  Eventually I want the build system (Rake/Switchtower/Capistrano?) doing this automatically by executing "python codes.rb" and redirecting output.  When we do that, this needs to be removed from hg and put in hgignore.

---
+++ shadowmanager/src/app/util/codes.rb
@@ -0,0 +1,21 @@
+# FIXME: eventually I want the build system to do this, but for now, it does not.
+# run python codes.py to get more recent constants for this file, and replace
+# the contents below.
+
+ERR_INTERNAL_ERROR = 5
+ERR_INVALID_ARGUMENTS = 6
+ERR_NO_SUCH_OBJECT = 7
+ERR_PASSWORD_INVALID = 4
+ERR_SUCCESS = 0
+ERR_TOKEN_EXPIRED = 1
+ERR_TOKEN_INVALID = 2
+ERR_UNCAUGHT_EXCEPTION = 999
+ERR_USER_INVALID = 3
+OP_ADD = 1
+OP_DELETE = 3
+OP_EDIT = 2
+OP_GET = 6
+OP_LIST = 4
+OP_METHOD = 5
+
+
