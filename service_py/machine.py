a8c33ca2904b6b4f59f507ffec765ac91595497b
Michael DeHaan <mdehaan@redhat.com>
2006-11-17 16:13:02 -0500

    Like image.py, only different

--- service_py/machine.py
+++ service_py/machine.py
@@ -15,15 +15,18 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 
 import time
-import base64
-from sqlalchemy import *
 from codes import *
 from errors import *
 import baseobj
 
 class Machine(baseobj.BaseObject):
 
-    def _produce(clss, args,operation=None):
+    def _produce(klass, args,operation=None):
+        """
+        Factory method.  Create a machine object from input data, optionally
+        running it through validation, which will vary depending on what
+        operation is creating the machine object.
+        """
         self = Machine()
         self.from_datastruct(args)
         self.validate(operation)
@@ -31,86 +34,154 @@ class Machine(baseobj.BaseObject):
     produce = classmethod(_produce)
 
     def from_datastruct(self,args):
-        self.id           = self.load(args,"id",-1)
-        self.machine      = self.load(args,"machine",-1)
-        self.architecture = self.load(args,"architecture",-1)
-        self.processors   = self.load(args,"processor_speed",-1)
-        self.processors   = self.load(args,"processor_count",-1)
-        self.memory       = self.load(args,"memory",-1)
-
+        """
+        Helper method to fill in the object's internal variables from
+        a hash.  Note that we *don't* want to do this and then call
+        session.save on the machine as the junk fields like the "-1" would be 
+        propogated.  It's best to use this for validation and build a *second*
+        machine object for interaction with the ORM.  See methods below for examples.
+        """
+        self.id               = self.load(args,"id",-1)
+        self.address          = self.load(args,"address",-1)
+        self.architecture     = self.load(args,"architecture",-1)
+        self.processor_speed  = self.load(args,"processor_speed",-1)
+        self.processor_count  = self.load(args,"processor_count",-1)
+        self.memory           = self.load(args,"memory",-1)
 
     def to_datastruct(self):
+        """
+        Serialize the object for transmission over WS.
+        """
         return {
-            "id"                : self.id,
-            "machine"           : self.machine,
-            "architecture"      : self.architecture,
-	    "processor_speed"   : self.processors,
-            "processor_count"   : self.processors,
-            "memory"            : self.memory
+            "id"              : self.id,
+            "address"         : self.address,
+            "architecture"    : self.architecture,
+            "processor_speed" : self.processor_speed,
+            "processor_count" : self.processor_count,
+            "memory"          : self.memory,
         }
 
-    def validate(self, operation):
+    def validate(self,operation):
+        """
+        Cast variables appropriately and raise InvalidArgumentException(["name of bad arg","..."])
+        if there are any problems.  Note that validation is operation specific, for instance
+        there is no ID for an "add" command because the add command generates the ID.
+
+        Note that getting python exception errors during a cast here is technically good enough
+        to prevent GIGO, but really InvalidArgumentsExceptions should be raised.  That's what
+        we want.
+
+        NOTE: API currently gives names of invalid fields but does not list reasons.  
+        i.e. (FALSE, INVALID_ARGUMENTS, ["foo,"bar"].  By making the 3rd argument a hash
+        it could, but these would also need to be codes.  { "foo" : OUT_OF_RANGE, "bar" : ... }
+        Up for consideration, but probably not needed at this point.  Can be added later. 
+        """
         # FIXME
         if operation in [OP_EDIT,OP_DELETE,OP_GET]:
             self.id = int(self.id)
 
-def machine_add(session,args):
-     machine = Machine.produce(args,OP_ADD) # force validation
-     machine.id = int(time.time()) # FIXME (?)
-     return machine_save(session,machine,args)
+def machine_add(websvc,args):
+     """
+     Create a machine.  args should contain all fields except ID.
+     """
+     u = Machine.produce(args,OP_ADD)
+     u.id = int(time.time())
+     st = """
+     INSERT INTO machines (id,address,architecture,processor_speed,processor_count,memory)
+     VALUES (:id,:address,:architecture,:processor_speed,:processor_count,:memory)
+     """
+     websvc.cursor.execute(st, u.to_datastruct())
+     websvc.connection.commit()
+     return success(u.id)
 
-def machine_edit(session,args):
-     temp_machine = Machine.produce(args,OP_EDIT) # force validation
-     query = session.query(Machine)
-     machine = query.get_by(Machine.c.id.in_(temp_machine.id))
-     if machine is None:
-        raise NoSuchObjectException()
-     return machine_save(session,machine,args)
+def machine_edit(websvc,args):
+     """
+     Edit a machine.  args should contain all fields that need to
+     be changed.
+     """
+     u = Machine.produce(args,OP_EDIT) # force validation
+     st = """
+     UPDATE machines 
+     SET machines.address=:address,
+     machines.architecture=:architecture,
+     machines.processor_speed=:processor_speed,
+     machines.processor_count=:processor_count,
+     machines.memory=:memory
+     WHERE machines.id=:id
+     """
+     websvc.cursor.execute(st, u.to_datastruct())
+     websvc.connection.commit()
+     return success(u.to_datastruct())
 
-def machine_save(session,machine,args):
-     # validation already done in methods calling this helper
-     machine.architecture = args["architecture"]
-     machine.processor_speed = args["processor_speed"]
-     machine.processor_count = args["processor_count"]
-     machine.address = args["address"]
-     machine.memory = args["memory"]
-     # FIXME: we'd want to do validation here (actually in the Machine class) 
-     session.save(machine)
-     session.flush()
-     ok = machine in session
-     if not ok:
-         raise InternalErrorException()
+def machine_delete(websvc,args):
+     """
+     Deletes a machine.  The args must only contain the id field.
+     """
+     u = Machine.produce(args,OP_DELETE) # force validation
+     st = """
+     DELETE FROM machines WHERE users.id=:id
+     """
+     websvc.cursor.execute(st, { "id" : u.id })
+     websvc.connection.commit()
+     # FIXME: failure based on existance
      return success()
 
-def machine_delete(session,args):
-     temp_machine = Machine.produce(args,OP_DELETE) # force validation
-     query = session.query(Machine)
-     machine = query.get_by(Machine.c.id.in_(temp_machine.id))
-     if machine is None:
-        return result(ERR_NO_SO_OBJECT)
-     session.delete(machine)
-     session.flush()
-     ok = not machine in session
-     if not ok:
-         raise InternalErrorException()
-     return success()
+def machine_list(websvc,args):
+     """
+     Return a list of machines.  The args list is currently *NOT*
+     used.  Ideally we need to include LIMIT information here for
+     GUI pagination when we start worrying about hundreds of systems.
+     """
+     offset = 0
+     limit  = 100
+     if args.has_key("offset"):
+        offset = args["offset"]
+     if args.has_key("limit"):
+        limit = args["limit"]
+     st = """
+     SELECT id,address,architecture,processor_speed,processor_count,memory
+     FROM users LIMIT ?,?
+     """ 
+     results = websvc.cursor.execute(st, (offset,limit))
+     results = websvc.cursor.fetchall()
+     machines = []
+     for x in results:
+         data = {         
+            "id"               : x[0],
+            "address"          : x[1],
+            "architecture"     : x[2],
+            "processor_speed"  : x[3],
+            "processor_count"  : x[4],
+            "memory"           : x[5],
+         }
+         machines.append(Machine.produce(data).to_datastruct())
+     return success(machines)
 
-def machine_list(session,args):
-     # no validation required
-     query = session.query(Machine)
-     sel = query.select()
-     list = [x.to_datastruct() for x in sel]
-     return success(list)
-
-def machine_get(session,args):
-     temp_machine = Machine.produce(args,OP_GET) # force validation
-     query = session.query(Machine)
-     machine = query.get_by(Machine.c.id.in_(temp_machine.id))
-     if machine is None:
-        raise NoSuchObjectException()
-     return success(machine.to_datastruct())
+def machine_get(websvc,args):
+     """
+     Return a specific machine record.  Only the "id" is required in args.
+     """
+     u = Machine.produce(args,OP_GET) # force validation
+     st = """
+     SELECT id,address,architecture,processor_speed,processor_count,memory
+     FROM machines WHERE id=?
+     """
+     websvc.cursor.execute(st,{ "id" : u.id })
+     x = websvc.cursor.fetchone()
+     data = {
+            "id"              : x[0],
+            "address"         : x[1],
+            "architecture"    : x[2],
+            "processor_speed" : x[3],
+            "processor_count" : x[4],
+            "memory"          : x[5],
+     }
+     return success(Machine.produce(data).to_datastruct())
 
 def register_rpc(handlers):
+     """
+     This adds RPC functions to the global list of handled functions.
+     """
      handlers["machine_add"]    = machine_add
      handlers["machine_delete"] = machine_delete
      handlers["machine_list"]   = machine_list
