cc1797ed9bed499fd846a90e8dceac0dc243195e
Michael DeHaan <mdehaan@redhat.com>
2006-11-30 17:08:46 -0500

    More cobbler integration, basically disabled until a few more pieces are written.
    NOTE: Defining of valid constants for certain values (i.e. architecture for a machine) and validating them will be required very soon.  Backend config file piece (to seed parts of /var/lib/cobbler/settings, among other things) will also need to be created.  Plus distro tools.  Lots of stuff...

--- service_py/provisioning.py
+++ service_py/provisioning.py
@@ -28,11 +28,54 @@ import machine
 import cobbler.api
 import shadow
 
+class CobblerTranslatedItem:
+   def add(self):
+       pass
+
+class CobblerTranslatedDistribution:
+   def __init__(self,api,from_db):
+       new_item = api.new_distro()
+       # new_item.set_name
+       # new_item.set_kernel
+       # new_item.set_initrd
+       # new_item.set_kernel_options
+       # new_item.set_arch
+       # new_item.set_ksmeta
+       api.distros().add(new_item)
+
+class CobblerTranslatedProfile:
+   def __init__(self,api,from_db):
+       new_item = api.new_image()
+       # new_item.set_name
+       # new_item.set_distro
+       # new_item.set_kickstart 
+       # new_item.set_kernel_options
+       # new_item.set_virt_name
+       # new_item.set_virt_file_size
+       # new_item.set_virt_ram
+       # new_item.set_ksmeta
+       api.profiles().add(new_item)
+
+class CobblerTranslatedSystem:
+   def __init__(self,api,from_db):
+       new_item = api.new_system()
+       # new_item.set_name
+       # new_item.set_profile
+       # new_item.set_kernel_options
+       # new_item.set_ksmeta
+       # new_item.set_pxe_paddress
+       api.systems().add(new_item)
+
+# FIXME: need another ShadowManager backend object that will need to sync with
+# /var/lib/cobbler/settings
+
 def provisioning_sync(websvc, args):
-     
-     distributions = distribution.distribution_list(websvc,{})
-     images        = image.image_list(websvc,{})
-     machines      = machine.machine_list(websvc,{})
+
+     return True # disable until later...     
+
+     (rc0, distributions) = distribution.distribution_list(websvc,{})
+     (rc1, images)        = image.image_list(websvc,{})
+     (rc2, machines)      = machine.machine_list(websvc,{})
 
      # cobbler can't be run multiple times at once...
      lock = threading.Lock()
@@ -40,10 +83,23 @@ def provisioning_sync(websvc, args):
 
      try:
          api = cobbler.api.BootAPI()
-         # api.serialize()
+         api.distros().clear()
+         api.profiles().clear()
+         api.systems().clear()
+         # cobbler can/will could raise exceptions on failure at any point...
+         # return code checking is not needed.
+         for d in distributions:
+             CobblerTranslatedDistribution(api,d).add()
+         for i in images:
+             CobblerTranslatedProfile(api,i).add()
+         for p in machines:
+             CobblerTranslatedSystem(api,p).add()
+         api.serialize()
+         api.sync()
      except:
          lock.release()
          raise UncaughtException(traceback.format_exc())
+ 
 
      lock.release()
      return success()
