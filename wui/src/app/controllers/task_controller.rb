94f871148ce33aeb4dc43ae79a0ea72aabe17f44
Michael DeHaan <mdehaan@redhat.com>
2007-01-09 12:46:18 -0500

    (A) renamed shadowmanager directory to "wui" to save typing, and shadowmanager is more than the wui scott: be sure to do a clean checkout when you get back :)
    (B) tweaking task controller
    (C) something else that I forgot.

---
+++ wui/src/app/controllers/task_controller.rb
@@ -0,0 +1,42 @@
+# Copyright 2007, Red Hat Inc
+# Michael DeHaan <mdehaan@redhat.com> 
+# Scott Seago <sseago@redhat.com>
+#
+#
+# This software may be freely redistributed under the terms of the GNU
+# general public license.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+# the task controller deals with background actions, some of which the user might be interested
+# in watching from the UI.  It should show state but allow only a minimum of editing (possibly
+# pausing a task) and that's it.
+#
+# the backend actually uses "action" for the word "task", but the word ActionController is 
+# apparently reserved by Rails.
+
+class TaskController < AbstractObjectController
+
+   def object_class
+       Task
+   end
+
+   def edit
+       @machines = ManagedObject.retrieve_all(Machine, @session).collect do |entry|
+           [ entry.name, entry.id ]
+       end
+       @images = ManagedObject.retrieve_all(Image, @session).collect do |entry|
+           [ entry.name, entry.id ]
+       end
+       @users = ManagedObject.retrieve_all(User, @session).collect do |entry|
+           [ entry.username, entry.id ]
+       end
+       @states = []
+       @operations = []
+
+       # FIXME: finish
+    end
+end
