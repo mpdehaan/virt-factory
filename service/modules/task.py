78010786f2a324d8e91bfa424cf8c8a3c506b4c8
Michael DeHaan <mdehaan@redhat.com>
2007-01-15 18:11:16 -0500

    Work on the task service ... definitely not finished.

--- service/modules/task.py
+++ service/modules/task.py
@@ -169,37 +169,5 @@ class Task(web_svc.AuthWebSvc):
        u = TaskData.produce(args, OP_GET) # validate
        return self.db.simple_get(args)
  
-   def do_background_thread(self, token, args):
-       
-       JUST_STARTED = """
-       task_results = self.list(token, {}) 
-       tasks = task_results.data 
-
-       # not a lot of tasks, no need for db sort...
-       def sorter(a,b):
-           return cmp(a["time"], b["time"])
- 
-       tasks.sort(sorter,reverse=True)
-       
-       for item in tasks:
-           item = TaskData.produce(item["data"])   
-           op = item.operation
-
-           # FIELDS = [ "id", "user_id", "operation", "parameters", "state", "time" ]
-
-           if op == TASK_OPERATION_COBBLER_SYNC:
-               pass
-           elif op == TASK_OPERATION_INSTALL_METAL:
-               pass
-           elif op == TASK_OPERATION_INSTALL_VIRT:
-               pass
-           elif op == TASK_OPERATION_PUPPET_SYNC:
-               pass
-           raise ShadowManagerException() # FIXME
-       """
-          
-
-
 methods = Task()
 register_rpc = methods.register_rpc
-
