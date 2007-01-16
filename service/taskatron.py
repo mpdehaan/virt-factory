27ccb40f262adc06d4640e6f728f7c6c1861b675
Michael DeHaan <mdehaan@redhat.com>
2007-01-16 12:38:25 -0500

    More work on taskatron.

--- service/taskatron.py
+++ service/taskatron.py
@@ -25,7 +25,7 @@ from codes import *
 import config_data
 import logger
 
-from modules import task 
+from modules import task as task_module 
 from modules import authentication
 from modules import config
 from modules import deployment
@@ -35,7 +35,6 @@ from modules import machine
 from modules import provisioning
 from modules import registration
 from modules import user
-
 
 #--------------------------------------------------------------------------
 
@@ -58,7 +57,25 @@ class TaskScheduler:
        # FIXME: use seperate log file (?)
        self.logger = logger.Logger().logger
 
-   def run(self):
+   def clean_up_tasks(self):
+       """
+       If we start up and any tasks are marked as "running", this means
+       taskatron died before they were finished.  Should they be marked
+       finished/stopped or queued?
+       FIXME: figure this out.
+       """
+       task_obj = task_module.Task()
+       task_results = task_obj.list(None, {})
+       tasks = task_results.data
+       for task in tasks:
+           item = task_module.TaskData.produce(task)
+           if item.state == TASK_STATE_RUNNING:
+               item.state = TASK_STATE_QUEUED
+               print "queuing unfinished task: %s" % item.to_datastruct()
+               task_obj.edit(None, item.to_datastruct())
+         
+
+   def run_forever(self):
        """
        Run forever.
        """
@@ -72,7 +89,7 @@ class TaskScheduler:
        one element, if not, process everything in the list.
        """
 
-       task_obj = task.Task()
+       task_obj = task_module.Task()
        task_results = task_obj.list(None, {})
        tasks = task_results.data
 
@@ -83,12 +100,15 @@ class TaskScheduler:
        tasks.sort(sorter,reverse=True)
 
        for item in tasks:
-           item = task.TaskData.produce(item)
+           print "---"
+           print "taskatron considering: %s" % item
+           item = task_module.TaskData.produce(item)
            op = item.operation
 
            task_lock = threading.Lock()
            context = TaskContext(self.logger, item, tasks, task_lock)
 
+           print "state : %s" % item.state
            if item.state == TASK_STATE_QUEUED:
                if op == TASK_OPERATION_COBBLER_SYNC:
                    worker = CobblerSyncThread(context)
@@ -100,22 +120,20 @@ class TaskScheduler:
                    worker = PuppetSyncThread(context)
                else:
                    raise TaskException(comment="unknown task type")
+               worker.start()
 
        task_results2 = task_obj.list(None, {})
        tasks = task_results2.data
+       print "==="
        for item in tasks:
            if item["state"] == TASK_STATE_FINISHED:
-               self.delete(None, item)
+               print "deleting: %s" % item
+               task_obj.delete(None, item)
        
        if once:
            return
 
 
-   def __log_exc(self):
-      (t, v, tb) = sys.exc_info()
-      self.logger.debug("Exception occured: %s" % t )
-      self.logger.debug("Exception value: %s" % v)
-      self.logger.debug("Exception Info:\n%s" % string.join(traceback.format_list(traceback.extract_tb(tb))))
      
 #-------------------------------------------------------------------------
 
@@ -154,17 +172,29 @@ class ShadowWorkerThread(threading.Thread):
         self.logger.debug("Running task  : %s" % self.item.id)
         self.logger.debug("   operation  : %s" % self.item.operation)
         self.logger.debug("  parameters  : %s" % self.item.parameters)
+        print "Running task  : %s" % self.item.id
+        print "   operation  : %s" % self.item.operation
+        print "  parameters  : %s" % self.item.parameters
        
         self.item.state = TASK_ITEM_RUNNING
-        self.edit(None, self.context.item.to_datastruct())
+        task_obj = task_module.Task()
+        task_obj.edit(None, self.item.to_datastruct())
 
     def set_finished(self):
         """
         Flag a task as not running in the database ... and log that too.
         """
         self.logger.debug("Finished task : %s" % self.item.id)
-        self.item.state = TASK_ITEM_FINISHED
-        self.edit(None, self.context.item.to_datastruct())
+        print "Finished task : %s" % self.item.id
+        self.item.state = TASK_STATE_FINISHED
+        task_obj = task_module.Task()
+        task_obj.edit(None, self.item.to_datastruct())
+
+    def log_exc(self):
+        (t, v, tb) = sys.exc_info()
+        self.logger.debug("Exception occured: %s" % t )
+        self.logger.debug("Exception value: %s" % v)
+        self.logger.debug("Exception Info:\n%s" % string.join(traceback.format_list(traceback.extract_tb(tb))))
  
 #--------------------------------------------------------------------------
 
@@ -181,20 +211,15 @@ class CobblerSyncThread(ShadowWorkerThread):
         # "provisioning" state... need to implement this.
 
         self.tasklock.acquire()
-
         try:
-
             self.set_running()
             prov_obj = provisioning.Provisioning()
             prov_obj.sync(None, {})
         except Exception, e:
-            self.__log_exc()
+            self.log_exc()
             self.tasklock.release()
-            self.join()
- 
         self.set_finished()
         self.tasklock.release()
-        self.join()
 
 #--------------------------------------------------------------------------
 
@@ -204,10 +229,20 @@ class CobblerInstallMetalThread(ShadowWorkerThread):
     Background thread for installing a bare metal system.
     """
 
-    def run(self, machine_id):
+    def run(self):
         # FIXME: make call to node to reprovision itself.
         # this does NOT need the lock.
-        self.join()
+        #self.tasklock.acquire()
+        try:
+            self.set_running()
+            # FIXME: do stuff here
+        except Exception, e:
+            self.log_exc()
+            #self.tasklock.release()
+        self.set_finished()
+        #self.tasklock.release()
+
+#--------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------
 
@@ -217,10 +252,19 @@ class CobblerInstallVirtThread(ShadowWorkerThread):
     Background thread for installing a virtualized system.
     """
 
-    def do_install_virt(self, deployment_id):
+    def run(self):
         # FIXME: make call to node to install virt image
         # this does NOT need the lock.
-        self.join()
+        #self.tasklock.acquire()
+        try:
+            self.set_running()
+            # FIXME: do stuff here
+        except Exception, e:
+            self.log_exc()
+            #self.tasklock.release()
+        self.set_finished()
+        #self.tasklock.release()
+
 
 #--------------------------------------------------------------------------
 
@@ -230,17 +274,18 @@ class PuppetSyncThread(ShadowWorkerThread):
     Background thread for updating the puppet config.
     """
 
-    def do_puppet_sync(self):
+    def run(self):
         # update puppet mappings to match database
-
-        self.tasklock.acquire()
-        self.set_running()
-
-        # FIXME: do puppet stuff here
-
+        #self.tasklock.acquire()
+        try:
+            self.set_running()
+            # FIXME: do stuff here
+        except Exception, e:
+            self.log_exc()
+            #self.tasklock.release()
         self.set_finished()
-        self.tasklock.release()
-        self.join()
+        #self.tasklock.release()
+
 
 #--------------------------------------------------------------------------
       
@@ -252,7 +297,8 @@ def main(argv):
     scheduler = TaskScheduler()     
 
     if len(argv) > 1 and argv[1].lower() == "--serve":
-        scheduler.run()
+        scheduler.clean_up_tasks()
+        scheduler.run_forever()
     elif len(argv) > 1 and argv[1].lower() == "--next":
         scheduler.tick()
     else:
