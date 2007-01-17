b59a54aef75853750cd13836954fe3e2a60f867a
mdehaan@mdehaan.rdu.redhat.com <>
2007-01-17 18:28:29 -0500

    Schema cleanup and added schema for registration_tokens

--- setup/schema.sql
+++ setup/schema.sql
@@ -1,89 +1,91 @@
 CREATE TABLE tasks (
-   id INTEGER PRIMARY KEY,
-   user_id INTEGER,
-   operation INTEGER NOT NULL,
-   parameters VARCHAR,
-   state INTEGER NOT NULL,
-   time INTEGER NOT NULL
+   id              INTEGER PRIMARY KEY,
+   user_id         INTEGER,
+   operation       INTEGER NOT NULL,
+   parameters      VARCHAR,
+   state           INTEGER NOT NULL,
+   time            REAL NOT NULL
 );
 
 CREATE TABLE users (
-   id INTEGER PRIMARY KEY,
-   username VARCHAR (255) UNIQUE NOT NULL,
-   password VARCHAR (255) NOT NULL,
-   first VARCHAR (255) NOT NULL,
-   middle VARCHAR (255),
-   last VARCHAR (255) NOT NULL,
-   description VARCHAR (255),
-   email VARCHAR (255) NOT NULL
+   id              INTEGER PRIMARY KEY,
+   username        VARCHAR (255) UNIQUE NOT NULL,
+   password        VARCHAR (255) NOT NULL,
+   first           VARCHAR (255) NOT NULL,
+   middle          VARCHAR (255),
+   last            VARCHAR (255) NOT NULL,
+   description     VARCHAR (255),
+   email           VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE events (
-   id INTEGER PRIMARY KEY,
-   time           INT NOT NULL,
-   user_id        INT NOT NULL,
-   machine_id     INT,
-   deployment_id  INT,
-   image_id       INT,
-   severity       INT NOT NULL,
-   category       VARCHAR (255) NOT NULL,
-   action         VARCHAR (255) NOT NULL,
-   user_comment   VARCHAR (255)
+   id              INTEGER PRIMARY KEY,
+   time            INTEGER NOT NULL,
+   user_id         INTEGER NOT NULL,
+   machine_id      INTEGER,
+   deployment_id   INTEGER,
+   image_id        INTEGER,
+   severity        INTEGER NOT NULL,
+   category        VARCHAR (255) NOT NULL,
+   action          VARCHAR (255) NOT NULL,
+   user_comment    VARCHAR (255)
 );
 
 CREATE TABLE distributions (
-   id INTEGER PRIMARY KEY,
-   kernel VARCHAR(255) NOT NULL,
-   initrd VARCHAR(255) NOT NULL,
-   options VARCHAR(255),
-   kickstart VARCHAR(255),
-   name VARCHAR(255) UNIQUE,
-   architecture INT NOT NULL,
-   kernel_options VARCHAR(255),       
-   kickstart_metadata VARCHAR(255)   
+   id                  INTEGER PRIMARY KEY,
+   kernel              VARCHAR(255) NOT NULL,
+   initrd              VARCHAR(255) NOT NULL,
+   options             VARCHAR(255),
+   kickstart           VARCHAR(255),
+   name                VARCHAR(255) UNIQUE,
+   architecture        INTEGER NOT NULL,
+   kernel_options      VARCHAR(255),       
+   kickstart_metadata  VARCHAR(255)   
 );
 
 CREATE TABLE images (
    id INTEGER PRIMARY KEY,
-   name       VARCHAR (255) UNIQUE,
-   version    VARCHAR (255) NOT NULL,
-   filename   VARCHAR (255),
-   specfile   VARCHAR (255),
-   distribution_id    INT NOT NULL,
-   virt_storage_size  INT NOT NULL,
-   virt_ram           INT NOT NULL,
+   name               VARCHAR (255) UNIQUE,
+   version            VARCHAR (255) NOT NULL,
+   filename           VARCHAR (255),
+   specfile           VARCHAR (255),
+   distribution_id    INTEGER NOT NULL,
+   virt_storage_size  INTEGER NOT NULL,
+   virt_ram           INTEGER NOT NULL,
    kickstart_metadata VARCHAR(255),
-   kernel_options VARCHAR(255),
-   valid_targets INT NOT NULL,
-   is_container INT NOT NULL 
+   kernel_options     VARCHAR(255),
+   valid_targets      INTEGER NOT NULL,
+   is_container       INTEGER NOT NULL 
 );
 
 CREATE TABLE deployments (
-   id INTEGER PRIMARY KEY,
-   machine_id    INT NOT NULL,
-   image_id      INT NOT NULL,
-   state         INT NOT NULL,
-   display_name VARCHAR(255) NOT NULL
+   id                 INTEGER PRIMARY KEY,
+   machine_id         INTEGER NOT NULL,
+   image_id           INTEGER NOT NULL,
+   state              INTEGER NOT NULL,
+   display_name       VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE machines (
    id                 INTEGER PRIMARY KEY,
    address            VARCHAR(255),
-   architecture       INT,
-   processor_speed    INT,
-   processor_count    INT,
-   memory             INT,
+   architecture       INTEGER,
+   processor_speed    INTEGER,
+   processor_count    INTEGER,
+   memory             INTEGER,
    kernel_options     VARCHAR(255),
    kickstart_metadata VARCHAR(255),
    list_group         VARCHAR(255),
    mac_address        VARCHAR(255),
-   is_container       INT,
-   image_id           INT
+   is_container       INTEGER,
+   image_id           INTEGER
 );
 
 CREATE TABLE registration_tokens (
-   id                 INT PRIMARY KEY,
+   id                 INTEGER PRIMARY KEY,
    token              VARCHAR(255),
-   image_id           INT 
+   image_id           INTEGER, 
+   user_id            INTEGER,
+   uses_remaining     INTEGER
 );
 
