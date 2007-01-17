2a76fab7d2dbdc4996ecb8ab16a529a404361980
Michael DeHaan <mdehaan@redhat.com>
2007-01-17 18:41:58 -0500

    Add registration tokens model code.

---
+++ wui/src/app/models/regtokens.rb
@@ -0,0 +1,27 @@
+# registration tokens are tokens that allow nodes to authenticate to the registration module for purpose
+# of self adding the machines.  They can have a limited use count or not.  They can be associated with
+# an image or not.  
+
+class RegTokens < ManagedObject
+
+    # corresponds with what's in the database
+    ATTR_LIST = { 
+        :id             => { :type => Integer }, 
+        :token          => { :type => String  }, 
+        :image_id       => { :type => Integer },
+        :user_id        => { :type => Integer }, 
+        :uses_remaining => { :type => Integer }, 
+        :image          => { :type => Image, :id_attr => :image_id } 
+        :user           => { :type => User, :id_attr => :user_id   }
+    }
+               
+    self.set_attrs(ATTR_LIST)
+
+    # web service methods start with this
+    METHOD_PREFIX = "regtokens"
+
+    def objname
+        id # correct?
+    end
+
+end
