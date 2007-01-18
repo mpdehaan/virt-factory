6b08889607076d18894882032ff3dc6d540b806b
Michael DeHaan <mdehaan@redhat.com>
2007-01-17 19:06:11 -0500

    Eliminate traceback on backend connect failure.

--- wui/src/app/controllers/regtoken_controller.rb
+++ wui/src/app/controllers/regtoken_controller.rb
@@ -1,2 +1,16 @@
-class RegtokenController < ApplicationController
+# the registration token controller is responsible for creating, listing, and removing registration
+# tokens that are used to enslave, I mean, register systems to shadowmanager.
+
+class RegtokenController < AbstractObjectController
+
+   def object_class
+       RegToken
+   end
+
+   def edit
+       super
+       # @images = ...
+   end
+
 end
+
