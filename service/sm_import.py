7f05c93d76226e649a728bc94352e0fe2817fc05
sseago@skagway.foo <>
2007-02-12 15:46:03 -0500

    pass options on to ShadowImporter

--- service/sm_import.py
+++ service/sm_import.py
@@ -72,13 +72,16 @@ class ShadowImporter:
    Main class for sm_import tool
    """
 
-   def __init__(self, tarball):
+   def __init__(self, tarball, module_dir, manifest, fileserver_conf):
        """
        create importer object -- source tarball is passed in
        """
        if (not tarfile.is_tarfile(tarball)):
            raise ValueError(tarball + " is not a tarfile")
        self.tarball_path = tarball
+       self.module_dir = module_dir
+       self.manifest = manifest
+       self.fileserver_conf = fileserver_conf
        log = logger.Logger()
        self.logger = log.logger
 
@@ -167,7 +170,8 @@ def main():
     (options, args) = parser.parse_args()
     if len(args) != 1:
         parser.error("incorrect number of arguments")    
-    importer = ShadowImporter(args[0])
+    importer = ShadowImporter(args[0], options.module, options.manifest,
+                              options.fileserver_conf)
     print "extracting..."
     importer.extract()
     print "populating..."
