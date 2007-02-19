97572812db85710d6e3e4f0f707ffdc3b9f2633b
Adrian Likins <alikins@redhat.com>
2007-02-19 13:19:27 -0500

    don't load modules that aren't you know, modules.

--- nodes/module_loader.py
+++ nodes/module_loader.py
@@ -26,6 +26,9 @@ def load_modules(module_path, blacklist=None):
 
         try:
             blip =  __import__("%s/%s" % (module_path, modname))
+            if not hasattr(blip, "register_rpc"):
+                print "%s/%s module not a proper module" % (module_path, modname) 
+                continue
             mods[modname] = blip
         except ImportError, e:
             print e
