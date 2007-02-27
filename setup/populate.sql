fc3652d46d099b8a9095b2a8ea3ff4113bb698ea
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-02-27 16:08:21 -0500

    Changing the token associated with the -1 record such that get_by_regtoken queries won't pick it up as a match when no regtoken is passed in.

--- setup/populate.sql
+++ setup/populate.sql
@@ -4,11 +4,11 @@ INSERT INTO distributions VALUES (-1, "*Unassigned*", "", "", "", "", 0, "", "")
 
 INSERT INTO profiles VALUES(-1, "*Unassigned*", "0.00", 1, 0, 0, "", "", -1, -1, "");
 
-INSERT INTO machines VALUES(-1, "*Unassigned*", "", "", 0, 0, 0, 0, "", "", "", "", 0, -1, "", 0, 0);
+INSERT INTO machines VALUES(-1, "*Unassigned*", "", "*NoSuchToken*", 0, 0, 0, 0, "", "", "", "", 0, -1, "", 0, 0);
 
 INSERT INTO regtokens VALUES(-1, "*Unassigned*", -1, NULL);
 
-INSERT INTO deployments VALUES(-1, "*Unassigned*", "", "", "", -1, -1, -1, "__NULL__", "", 0, 0);
+INSERT INTO deployments VALUES(-1, "*Unassigned*", "", "*NoSuchToken*", "", -1, -1, -1, "__NULL__", "", 0, 0);
 
 
 
