f24d0b0ade538b3663d6d773a7e7f6f04cf57a5b
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-04-12 18:09:02 -0400

    Registration fix.

--- register/register/register.py
+++ register/register/register.py
@@ -83,7 +83,7 @@ class Register(object):
             fd4 = open("/etc/sysconfig/virt-factory/profile", "w+")
             fd4.write(profile_name)
             fd4.close()
-            server = server_url.split('/')[2]
+            server = self.server_url.split('/')[2]
             server = server.split(':')[0]
             self.update_puppet_sysconfig(server)
             puppetcmd = "/usr/sbin/puppetd --waitforcert 0 --server " + server + " --test"
