1af076adbd3bc2c9f203d65c370d7b0d75b8d0b8
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-04-27 16:53:37 -0400

    Re-enable the repo management features as they are kind of important

--- service/modules/provisioning.py
+++ service/modules/provisioning.py
@@ -234,7 +234,7 @@ class CobblerTranslatedProfile:
 
        ks_meta["cryptpw"]              = "$1$mF86/UHC$WvcIcX2t6crBz2onWxyac." # FIXME
        ks_meta["token_param"]          = "--token=UNSET" # intentional, system can override
-       ks_meta["repo_line"]  = "repo --name=vf_repo --baseurl http://%s/vf_repo" % vf_config["this_server"]["address"]
+       ks_meta["repo_line"]  = "repo --name=vf_repo --baseurl http://%s/cobbler/repo_mirror/vf_repo" % vf_config["this_server"]["address"]
        ks_meta["server_param"] = "--server=http://%s:5150" % vf_config["this_server"]["address"] 
        ks_meta["server_name"] = vf_config["this_server"]["address"] 
 
@@ -516,11 +516,10 @@ class Provisioning(web_svc.AuthWebSvc):
         print NOW_IMPORTING
 
         # deal with repositories first... as the profiles might reference them.
-        #for repo_name in vf_config["repos"].keys():
-        #   mirror_url = vf_config["repos"][repo_name]
-        #   CobblerTranslatedRepo(cobbler_api,repo_name,mirror_url)
-        # FIXME
-        #cobbler_api.reposync() 
+        for repo_name in vf_config["repos"].keys():
+           mirror_url = vf_config["repos"][repo_name]
+           CobblerTranslatedRepo(cobbler_api,repo_name,mirror_url)
+        cobbler_api.reposync() 
 
         # read the config entry to find out cobbler's mirror locations
         for mirror_name in vf_config["mirrors"]:
