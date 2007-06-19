e3655c54f5f3d52efce7e3b9e9ff376ec0fa5a6c
Michael DeHaan <mdehaan@mdehaan.rdu.redhat.com>
2007-06-19 15:27:11 -0400

    machine.data not data

--- service/modules/machine.py
+++ service/modules/machine.py
@@ -77,7 +77,7 @@ class Machine(web_svc.AuthWebSvc):
             session.save(machine)
             session.flush()
             if machine.profile_id >= 0:
-                self.cobbler_sync(data) 
+                self.cobbler_sync(machine.data) 
             return codes.success(machine.id)
         finally:
             session.close()
