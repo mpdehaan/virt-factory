902660bd57dbfd76734bd19434173b19641b5cec
Adrian Likins <alikins@grimlock.devel.redhat.com>
2007-06-21 15:30:20 -0400

    state and display dont need to be verified yet

--- service/modules/deployment.py
+++ service/modules/deployment.py
@@ -114,10 +114,14 @@ class Deployment(web_svc.AuthWebSvc):
         """
         mac = None
         profilename = None
-        required = ('machine_id', 'profile_id', 'state', 'display_name')
-        optional =\
-            ('hostname', 'ip_address', 'registration_token', 'mac_address', 'netboot_enabled', 
-             'puppet_node_diff', 'is_locked')
+        required = ('machine_id', 'profile_id')
+        optional = ('hostname',
+                    'ip_address',
+                    'registration_token',
+                    'mac_address',
+                    'netboot_enabled', 
+                    'puppet_node_diff',
+                    'is_locked')
         validator = FieldValidator(args)
         validator.verify_required(required)
         validator.verify_printable('puppet_node_diff')
@@ -199,7 +203,7 @@ class Deployment(web_svc.AuthWebSvc):
         mac = None
         profilename = None
         required = ('id',)
-        optional =\
+        optional =
             ('machine_id', 'state', 'display_name', 'hostname', 'ip_address', 'registration_token',
              'mac_address', 'netboot_enabled', 'puppet_node_diff', 'is_locked')
         filter = ('id', 'profile_id')
