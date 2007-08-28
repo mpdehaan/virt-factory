5eaf75540b75220053b2367c3427955a0c2bcb48
Adrian Likins <alikins@grimlock.devel.redhat.com>
2007-08-28 17:00:32 -0400

    add code to export deployment api

--- ampm/api_modules/deployment.py
+++ ampm/api_modules/deployment.py
@@ -10,7 +10,27 @@ class Deployment(base_module.BaseModule):
         self.server = server
         base_module.BaseModule.__init__(self)
         self.methods = ["deployment_list", "deployment_get",
-                        "deployment_add", "deployment_get_by_mac"]
+                        "deployment_add", "deployment_get_by_mac",
+                        "deployment_pause", "deployment_unpause",
+                        "deployment_start", "deployment_stop",
+                        "deployment_shutdown", "deployment_destroy",
+                        "deployment_list"]
+                     
+
+    def deployment_pause(self, profile_id):
+        return self.server.deployment_pause(self.token, {'id' : profile_id})
+
+    def deployment_unpause(self, profile_id):
+        return self.server.deployment_unpause(self.token, {'id' : profile_id})
+
+    def deployment_shutdown(self, profile_id):
+        return self.server.deployment_shutdown(self.token, {'id' : profile_id})
+
+    def deployment_destroy(self, profile_id):
+        return self.server.deployment_destroy(self.token, {'id' : profile_id})
+
+    def deployment_start(self, profile_id):
+        return self.server.deployment_start(self.token, {'id': profile_id})
 
     def deployment_list(self):
         return self.server.deployment_list(self.token,{})
