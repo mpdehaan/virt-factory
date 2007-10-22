9202bc26eb5296a923dc402b71e9103d5cffc2dc
Scott Seago <sseago@redhat.com>
2007-10-22 11:53:17 -0400

    typo in upgrade script

--- service/db/schema/upgrade/0003.py
+++ service/db/schema/upgrade/0003.py
@@ -7,17 +7,17 @@ from datetime import datetime
 meta = BoundMetaData(migrate_engine)
 tables = []
 
-Database.tables.append(Table('tags', Database.meta,
+tables.append(Table('tags', Database.meta,
     Column('id', Integer, Sequence('tagid'), primary_key=True),
     Column('name', String(255), unique=True)
 ))
         
-Database.tables.append(Table('machine_tags', Database.meta,
+tables.append(Table('machine_tags', Database.meta,
     Column('tag_id', Integer, ForeignKey('tags.id')),
     Column('machine_id', Integer, ForeignKey('machines.id'))
 ))
         
-Database.tables.append(Table('deployment_tags', Database.meta,
+tables.append(Table('deployment_tags', Database.meta,
     Column('tag_id', Integer, ForeignKey('tags.id')),
     Column('deployment_id', Integer, ForeignKey('deployments.id'))
 ))
